d2241493ab8b95965d6c8e61421503e5132e3e7c
Ambiorix Martinez <ambiorix565@gmail.com>
2024-06-17 18:23:12 -0400

    Ordenes / Firebase

--- src/components/CheckoutComponent/CheckoutComponent.jsx
+++ src/components/CheckoutComponent/CheckoutComponent.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import "./CheckoutComponent.css";
+import { addOrder } from "../../firebase";
+import { CartContextComponent} from '../CartContextComponent/CartContextComponent';
+import PropTypes from "prop-types";
+
 
 export default function CheckoutComponent(){
-    const [formInputs, setFormInputs] = useState({ nombre:"", apellido:"", correo:"", })
+    const [formInputs, setFormInputs] = useState({ name:"", lastName:"", email:"", phone:"", confirmEmail:"" })
+    const {cart, vaciarCarrito, calcularTotal} = useContext(CartContextComponent)
+    const totalPrice = calcularTotal().toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
 
     //Manejo de los inputs
     const handleinputs = (e) => {
@@ -12,21 +19,37 @@ export default function CheckoutComponent(){
         });
     };
 
-    //Manejo del envio del formulario
-    const handleSubmit = (e) => {
+    //Manejo del envio del formulario a firebase
+    const handleSubmit = async (e) => {
         e.preventDefault();
         //validaciones de los campos
-        if(!formInputs.nombre || !formInputs.apellido || !formInputs.correo){
+        if(!formInputs.name || !formInputs.lastName || !formInputs.phone || !formInputs.email){
             alert("¡Favor de llenar todos los campos!")
             return;
         }
-        if (formInputs.correo !== formInputs.confirmCorreo){
+        if (formInputs.email !== formInputs.confirmEmail){
             alert("¡Los correos no coinciden!")
             return;
         } 
         console.log("Datos del formulario: ", formInputs)
-        
-        //colocar los datos de envio para la Base de datos
+
+        //Para guardar toda la info del formulario sin confirmEmail
+        // eslint-disable-next-line no-unused-vars
+        const { confirmEmail, ...orderData } = formInputs;
+
+        // Para guardar toda la info de cart en una nueva lista pero sin la propiedad img
+        // eslint-disable-next-line no-unused-vars
+        const cartItems = cart.map(({img, ...item}) => item)
+
+        //Envio de los datos del formulario y carrito para la Base de datos
+       await addOrder(orderData, cartItems, totalPrice).then((id) => {
+            alert(`Orden generada con éxito. ID: ${id}`);
+            vaciarCarrito();
+
+            //Para redirigir o limpiar el formulario
+            setFormInputs({ name: "", lastName: "", email: "", phone: "", confirmEmail:"" });
+
+        });
     }
 
     return(
@@ -37,54 +60,71 @@ export default function CheckoutComponent(){
                     <div className="formulario">
                     <h2 >Formulario para generar la orden</h2>
                         <div className="form-floating mb-1">
-                            <label htmlFor="nombre" >Nombres:</label>
+                            <label htmlFor="name" >Nombres:</label>
                             <br />
                             <input
                                 className="form-control"
                                 type="text"
-                                id="nombre"
-                                name="nombre"
-                                value={formInputs.nombre}
+                                id="name"
+                                name="name"
+                                value={formInputs.name}
                                 onChange={handleinputs}
                             />
                         </div>
                         <div className="form-floating mb-1">
-                            <label htmlFor="apellido">Apellidos:</label>
+                            <label htmlFor="lastName">Apellidos:</label>
                             <br />
                             <input
                                 className="form-control"
                                 type="text"
-                                id="apellido"
-                                name="apellido"
-                                value={formInputs.apellido}
+                                id="lastName"
+                                name="lastName"
+                                value={formInputs.lastName}
                                 onChange={handleinputs}
                             />
                         </div>
                         <div className="form-floating mb-1">
-                            <label htmlFor="correo">Correo:</label>
+                            <label htmlFor="phone">Telefono o celular:</label>
                             <br />
                             <input
                                 className="form-control"
-                                type="email"
-                                id="correo"
-                                name="correo"
-                                value={formInputs.correo}
+                                type="tel"
+                                id="phone"
+                                name="phone"
+                                value={formInputs.phone}
                                 onChange={handleinputs}
+                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                placeholder="849-123-4567"
+                                required
                             />
                         </div>
                         <div className="form-floating mb-1">
-                            <label htmlFor="confirmCorreo">Confirmar Correo:</label>
+                            <label htmlFor="email">Correo:</label>
                             <br />
                             <input
                                 className="form-control"
                                 type="email"
-                                id="confirmCorreo"
-                                name="confirmCorreo"
-                                value={formInputs.confirmCorreo}
+                                id="email"
+                                name="email"
+                                value={formInputs.email}
+                                onChange={handleinputs}
+                                placeholder="correo@correo.com"
+                            />
+                        </div>
+                        <div className="form-floating mb-1">
+                            <label htmlFor="confirmEmail">Confirmar Correo:</label>
+                            <br />
+                            <input
+                                className="form-control"
+                                type="email"
+                                id="confirmEmail"
+                                name="confirmEmail"
+                                value={formInputs.confirmEmail}
                                 onChange={handleinputs}
                             />
                         </div>
                         <button className="btn btn-primary w-100 py-2" type="submit">Enviar nueva orden</button>
+
                     </div>
                 </form>
             </div>
@@ -93,3 +133,8 @@ export default function CheckoutComponent(){
     )
 
 }
+
+CheckoutComponent.propTypes = {
+    confirmEmail: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+  };
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, getFirestore, addDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +21,7 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
+//funcion para ver mostrar los productos
 export async function getProducts(){
     const response = await getDocs(collection(db, "Products"));
     const listaProds=[];
@@ -28,6 +29,7 @@ export async function getProducts(){
     return listaProds;
 }
 
+//funcion para ver los detalles de los productos
 export async function getProduct(id){
     const docRef = doc(db, "Products", id);
     const response = await getDoc(docRef); 
@@ -39,4 +41,17 @@ export async function getProduct(id){
     }
     
 }
+
+
+//Generar una orden
+export async function addOrder(orderData, cartItems, total) {
+    const ordersCollection = collection(db, 'Orders');
+    const neworder = {
+        ...orderData, items: cartItems, total, date: new Date(),
+    };
+    const docRef = await addDoc(ordersCollection, neworder);
+    console.log('Doc ref generado: ' + docRef);
+    console.log('Id generado: ' + docRef.id);
+    return docRef.id;
+  }
 
