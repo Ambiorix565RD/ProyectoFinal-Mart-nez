e57c61ca94f79b6beca5253d67ba801c8b11863a
Ambiorix Martinez <ambiorix565@gmail.com>
2024-07-09 17:45:29 -0400

    Form

--- src/components/CheckoutComponent/CheckoutComponent.jsx
+++ src/components/CheckoutComponent/CheckoutComponent.jsx
@@ -14,24 +14,140 @@ export default function CheckoutComponent(){
     //Manejo de los inputs
     const handleinputs = (e) => {
         const {name, value} = e.target;
-        setFormInputs({
-            ...formInputs, [name]: value
-        });
+
+        // manejo del campo telefono o celular para aplicar guiones automaticamente a medida el cliente va llenando el campo telefono o celular y no pueda colocar letras.
+        if(name === "phone"){
+            
+            const numericValue = value.replace(/\D/g, "");
+
+            let formattedValue = numericValue;
+            if(numericValue.length > 3){
+                formattedValue = `${numericValue.slice(0,3)}-${numericValue.slice(3)}`;
+            }
+
+            if(numericValue.length > 6){
+                formattedValue = `${numericValue.slice(0,3)}-${numericValue.slice(3,6)}-${numericValue.slice(6, 10)}`;
+            }
+
+            setFormInputs({
+                ...formInputs, [name]: formattedValue
+            });
+
+            //Limitar la cantidad de numeros que puede introducir para que sea de acuerdo al formato del pais.
+            if(numericValue.length > 10){
+                formattedValue = formattedValue.slice(0,12);
+            }
+
+        } else if (name === "name" || name === "lastName"){
+
+            //validar que no ingresen numero en el campo de name y lastName
+
+            const onlyLetters = /^[a-zA-Z\s]*$/;
+            if(onlyLetters.test(value)) {
+                setFormInputs({
+                    ...formInputs, [name]: value
+                });
+            }
+        } else {
+
+            setFormInputs({
+                ...formInputs, [name]: value
+            });
+        }
     };
+
+    const validateName = (name) => {
+        const words = /^[a-zA-Z\s]*$/;
+        return words.test(name);
+    }
+
+    const validatePhone = (phone) => {
+        const number = /[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+        return number.test(phone);
+    }
+
+    const handleConfirmEmailBlur = () => {
+        if (formInputs.email !== formInputs.confirmEmail){
+            Swal.fire({
+                title: "¡Los correos no coinciden!",
+                icon: 'error',
+                confirmButtonColor: "#3085d6",
+                confirmButtonText: 'OK'
+            })
+            return;
+        } 
+    }
+
+    //prevenir que copie y pegue el email en confirmEmail
+    const handleConfirmEmailPage = (e) => {
+        e.preventDefault();
+        return false;
+    }
 
     //Manejo del envio del formulario a firebase
     const handleSubmit = async (e) => {
         e.preventDefault();
         //validaciones de los campos
         if(!formInputs.name || !formInputs.lastName || !formInputs.phone || !formInputs.email){
-            alert("¡Favor de llenar todos los campos!")
+            
+            Swal.fire({
+                title: "¡Favor de llenar todos los campos!",
+                icon: 'error',
+                confirmButtonColor: "#3085d6",
+                confirmButtonText: 'OK'
+            })
             return;
         }
-        if (formInputs.email !== formInputs.confirmEmail){
-            alert("¡Los correos no coinciden!")
-            return;
-        } 
 
+        //Validar que se coloque numeros en el campo nombre 
+        if(!validateName(formInputs.name)){
+               
+            Swal.fire({
+                title: "¡El nombre no debe contener números!",
+                icon: 'error',
+                confirmButtonColor: "#3085d6",
+                confirmButtonText: 'OK'
+            })
+            return;
+            
+        }
+
+        //Validar que se coloque numeros en el campo apellido
+        if(!validateName(formInputs.lastName)){
+                
+            Swal.fire({
+                title: "¡El apellido no debe contener números!",
+                icon: 'error',
+                confirmButtonColor: "#3085d6",
+                confirmButtonText: 'OK'
+            })
+            return;
+            
+        }
+
+
+        if(!validatePhone (formInputs.phone)){
+            Swal.fire({
+                title: "¡Favor colocar un número de teléfono válido!",
+                icon: 'error',
+                confirmButtonColor: "#3085d6",
+                confirmButtonText: 'OK'
+            })
+            return;
+        }
+
+        //Validar que se concuerden los campos email y la confirmacion del email
+        
+            if (formInputs.email !== formInputs.confirmEmail){
+                Swal.fire({
+                    title: "¡Los correos no coinciden!",
+                    icon: 'error',
+                    confirmButtonColor: "#3085d6",
+                    confirmButtonText: 'OK'
+                })
+                return;
+            } 
+        
         //Para guardar toda la info del formulario sin confirmEmail
         // eslint-disable-next-line no-unused-vars
         const { confirmEmail, ...orderData } = formInputs;
@@ -71,9 +187,8 @@ export default function CheckoutComponent(){
                 <form onSubmit={handleSubmit}>
                     <div className="formulario">
                     <h2 >Formulario para generar la orden</h2>
-                        <div className="form-floating mb-1">
+                        <div className="form-group mb-1">
                             <label htmlFor="name" >Nombres:</label>
-                            <br />
                             <input
                                 className="form-control"
                                 type="text"
@@ -81,11 +196,13 @@ export default function CheckoutComponent(){
                                 name="name"
                                 value={formInputs.name}
                                 onChange={handleinputs}
+                                placeholder="Ingrese su nombre"
+                                required
                             />
                         </div>
-                        <div className="form-floating mb-1">
+                        <div className="form-group mb-1">
                             <label htmlFor="lastName">Apellidos:</label>
-                            <br />
+                            
                             <input
                                 className="form-control"
                                 type="text"
@@ -93,11 +210,12 @@ export default function CheckoutComponent(){
                                 name="lastName"
                                 value={formInputs.lastName}
                                 onChange={handleinputs}
+                                placeholder="Ingrese su apellido"
+                                required
                             />
                         </div>
-                        <div className="form-floating mb-1">
-                            <label htmlFor="phone">Telefono o celular:</label>
-                            <br />
+                        <div className="form-group mb-1">
+                            <label htmlFor="phone">Teléfono o celular:</label>
                             <input
                                 className="form-control"
                                 type="tel"
@@ -105,14 +223,12 @@ export default function CheckoutComponent(){
                                 name="phone"
                                 value={formInputs.phone}
                                 onChange={handleinputs}
-                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                                 placeholder="849-123-4567"
                                 required
                             />
                         </div>
-                        <div className="form-floating mb-1">
+                        <div className="form-group mb-1">
                             <label htmlFor="email">Correo:</label>
-                            <br />
                             <input
                                 className="form-control"
                                 type="email"
@@ -121,11 +237,11 @@ export default function CheckoutComponent(){
                                 value={formInputs.email}
                                 onChange={handleinputs}
                                 placeholder="correo@correo.com"
+                                required
                             />
                         </div>
-                        <div className="form-floating mb-1">
+                        <div className="form-group mb-1">
                             <label htmlFor="confirmEmail">Confirmar Correo:</label>
-                            <br />
                             <input
                                 className="form-control"
                                 type="email"
@@ -133,10 +249,13 @@ export default function CheckoutComponent(){
                                 name="confirmEmail"
                                 value={formInputs.confirmEmail}
                                 onChange={handleinputs}
+                                onBlur={handleConfirmEmailBlur}
+                                onPaste={handleConfirmEmailPage}
+                                placeholder="Confirme su correo"
+                                required
                             />
                         </div>
                         <button className="btn btn-primary w-100 py-2" type="submit">Enviar nueva orden</button>
-
                     </div>
                 </form>
             </div>
